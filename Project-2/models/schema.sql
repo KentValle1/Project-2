3aae8e84c38cb3af644e111fa009396a70368557
josemm83 <josemmancha@gmail.com>
2020-03-28 10:20:45 -0500

    updated files to make the page load

--- Project-2/models/schema.sql
+++ Project-2/models/schema.sql
@@ -1,5 +1,83 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS album_genresDB;
+CREATE database album_genresDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE album_genresDB;
+
+
+CREATE TABLE ROCK (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE POP (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE COUNTRY (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE ELECTRONIC (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE METAL (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE HIPHOP (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE BLUESSOUL (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  date VARCHAR(100) NULL,
+  info TEXT(1000) NULL,
+  like_artists VARCHAR(400) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM ROCK;
+SELECT * FROM POP;
+SELECT * FROM COUNTRY;
+SELECT * FROM ELECTRONIC;
+SELECT * FROM METAL;
+SELECT * FROM HIPHOP;
+SELECT * FROM BLUESSOUL;
